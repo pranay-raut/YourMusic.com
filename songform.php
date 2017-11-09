7e638817d4559e1a6c6646c7632d06858df70db6
pranay-raut <33520424+pranay-raut@users.noreply.github.com>
2017-11-09 23:25:20 +0530

    YourMusic.com is a music website.
    Here you could find songs of new movies. You can upload as well as download or view the songs list of particular films

---
+++ songform.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html>
+<head>
+</head>
+<body>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="audioFile"/>
+<input type="submit" value="Upload Audio" name="save_audio"/>
+</form>
+</body>
+</html>
